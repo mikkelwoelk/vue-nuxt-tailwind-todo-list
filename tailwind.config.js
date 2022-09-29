7c6a4abf99e7efa8d0c5d71a914806eaac4550e1
Mikkel Wølk <mikkelwolk@gmail.com>
2022-09-29 11:30:43 +0200

    Initiated tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './components/**/*.{js,vue}',
+        './layouts/**/*.vue',
+        './pages/**/*.vue',
+        './plugins/**/*.js',
+        './nuxt.config.js',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
