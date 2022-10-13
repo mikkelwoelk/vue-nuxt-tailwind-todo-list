2c130cdf2fb2eca2dc7cad2722a2ba525ad3653b
Mikkel Wølk <mikkelwolk@gmail.com>
2022-10-13 13:46:29 +0200

    Styled HeaderMenu

---
+++ components/HeaderMenu.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="c-header-menu fixed w-full p-4 top-0 left-0 z-50">
+        <ul
+            class="c-header-menu__container h-full flex items-center justify-center"
+        >
+            <li
+                v-for="page in pages"
+                :key="page.name"
+                class="p-4 mx-4 text-3xl text-white capitalize"
+            >
+                <nuxt-link :to="`/${page.link}`"> {{ page.name }} </nuxt-link>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            pages: [
+                { name: 'home', link: '' },
+                { name: 'archive', link: 'archive' },
+                { name: 'more', link: 'more' },
+            ],
+        }
+    },
+}
+</script>
+
+<style scoped>
+.c-header-menu {
+    background-color: var(--clr-secondary);
+}
+</style>
