54ec439cb8a8fa2c8391dcb7d7fea6937ec04a46
Mikkel Wølk <mikkelwolk@gmail.com>
2022-10-17 10:46:27 +0200

    Updated export to use content instead of purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: [
+    content: [
         './components/**/*.{js,vue}',
         './layouts/**/*.vue',
         './pages/**/*.vue',
