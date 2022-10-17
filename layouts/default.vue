497c4de98eae6babf07d767b5ac95ce94bd689e9
Mikkel Wølk <mikkelwolk@gmail.com>
2022-10-17 10:06:52 +0200

    Added new default layout to accommodate for navigation bar

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <HeaderMenu />
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+import HeaderMenu from '../components/HeaderMenu.vue'
+export default {
+    components: { HeaderMenu },
+}
+</script>
