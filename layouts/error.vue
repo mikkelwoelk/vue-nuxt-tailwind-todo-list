b7f3f710d5cea716cb7e0e067e08a34d098984b7
Mikkel Wølk <mikkelwolk@gmail.com>
2022-11-02 14:50:28 +0100

    Added custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-screen w-screen">
+        <h1 class="text-7xl font-bold leading-normal">
+            This page doesn't exist
+        </h1>
+        <h2 class="text-3xl">
+            Why don't we just head on
+            <nuxt-link to="/" class="italic underline">back</nuxt-link>
+        </h2>
+    </div>
+</template>
