5c29af739800e52918bca111a9a44329ec8d6d0a
Mikkel Wølk <mikkelwolk@gmail.com>
2022-11-02 15:48:18 +0100

    Changed some text on the error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex flex-col items-center justify-center h-screen w-screen">
         <h1 class="text-7xl font-bold leading-normal">
-            This page doesn't exist
+            There is nothing ToDo here
         </h1>
         <h2 class="text-3xl">
             Why don't we just head on
-            <nuxt-link to="/" class="italic underline">back</nuxt-link>
+            <nuxt-link to="/" class="italic underline">home</nuxt-link>
         </h2>
     </div>
 </template>
